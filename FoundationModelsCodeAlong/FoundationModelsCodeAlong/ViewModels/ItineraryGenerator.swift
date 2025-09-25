5262436e07714d9acea845069ddd15a838a3be31
Alex Faria <alexfaria@iMac.Home>
2025-09-25 19:18:39 +0100

    Chapter 4

--- FoundationModelsCodeAlong/FoundationModelsCodeAlong/ViewModels/ItineraryGenerator.swift
+++ FoundationModelsCodeAlong/FoundationModelsCodeAlong/ViewModels/ItineraryGenerator.swift
@@ -17,8 +17,7 @@ final class ItineraryGenerator {
     
     private var session: LanguageModelSession
     
-    // MARK: - [CODE-ALONG] Chapter 4.1.1: Change the property to hold a partially generated Itinerary
-    private(set) var itinerary: Itinerary?
+    private(set) var itinerary: Itinerary.PartiallyGenerated?
 
     init(landmark: Landmark) {
         self.landmark = landmark
@@ -31,7 +30,6 @@ final class ItineraryGenerator {
     }
 
     func generateItinerary(dayCount: Int = 3) async {
-        // MARK: - [CODE-ALONG] Chapter 4.1.2: Update to use streaming API
         // MARK: - [CODE-ALONG] Chapter 5.3.3: Update `session.streamResponse` to include greedy sampling
         // MARK: - [CODE-ALONG] Chapter 6.2.1: Update to exclude schema from prompt
         do {
@@ -41,8 +39,12 @@ final class ItineraryGenerator {
                             "Here is an example of the desired format, but don't copy its content:"
                             Itinerary.exampleTripToJapan
                         }
-            let response = try await session.respond(to: prompt, generating: Itinerary.self)
-            self.itinerary = response.content
+            let stream = session.streamResponse(to: prompt,
+                                                 generating: Itinerary.self)
+             for try await partialResponse in stream {
+                 self.itinerary = partialResponse.content
+             }
+
         } catch {
             self.error = error
         }
